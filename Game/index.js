e1b2aa490ceff864df467a2b7c60782ec7651c28
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-10-26 22:48:28 +0300

    added animations for characters, added dragon monster with animations

--- Game/index.js
+++ Game/index.js
@@ -26,13 +26,21 @@ function preload()
     this.load.atlas('knight', '../Game/Assets/knight/spritesheet.png', '../Game/Assets/knight/spritesheet.json');
     this.load.atlas('mage', '../Game/Assets/mage/spritesheet.png', '../Game/Assets/mage/spritesheet.json');
     this.load.atlas('rogue', '../Game/Assets/rogue/spritesheet.png', '../Game/Assets/rogue/spritesheet.json');
+    this.load.atlas('dragon', '../Game/Assets/dragon/dragon.png', '../Game/Assets/dragon/spridragontesheet.json');
 }
 
 function create(){
     animname = 'knight'
     player = this.physics.add.sprite(100, 450, animname);
     player.setCollideWorldBounds(true);
-    this.physics.gravity=0
+
+//player animations
+    this.anims.create({
+        key:'idle',
+        frames:[{key:animname,frame:'idle.png'}],
+        frameRate:8,
+        repeat:-1,
+    })
     this.anims.create({
         key: 'walk',
         frames: [
@@ -47,11 +55,79 @@ function create(){
         repeat: -1,
     });
     this.anims.create({
-        key:'idle',
-        frames:[{key:animname,frame:'Knight.png'}],
-        frameRate:8,
-        repeat:-1,
-    })
+        key: 'push',
+        frames: [
+            { key: animname, frame: 'push1.png' },
+            { key: animname, frame: 'push2.png' },
+            { key: animname, frame: 'push3.png' },
+            { key: animname, frame: 'push4.png' }
+        ],
+        frameRate: 8,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'hurt',
+        frames: [
+            { key: animname, frame: 'hurt1.png' },
+            { key: animname, frame: 'hurt2.png' },
+            { key: animname, frame: 'hurt3.png' },
+            { key: animname, frame: 'hurt4.png' }
+        ],
+        frameRate: 8,
+        repeat: -1,
+    });
+    this.anims.create({
+        key: 'death',
+        frames: [
+            { key: animname, frame: 'death1.png' },
+            { key: animname, frame: 'death2.png' },
+            { key: animname, frame: 'death3.png' },
+            { key: animname, frame: 'death4.png' },
+            { key: animname, frame: 'death5.png' },
+            { key: animname, frame: 'death6.png' },
+            { key: animname, frame: 'death7.png' }
+        ],
+        frameRate: 8,
+        repeat: -1,
+    });
+    
+//dragon animation
+this.anims.create({
+    key: 'dragon_run',
+    frames: [
+        { key: 'dragon', frame: 'dragon_knight_run-0.png' },
+        { key: 'dragon', frame: 'dragon_knight_run-1.png' },
+        { key: 'dragon', frame: 'dragon_knight_run-2.png' },
+        { key: 'dragon', frame: 'dragon_knight_run-3.png' }
+    ],
+    frameRate: 8,
+    repeat: -1,
+});
+this.anims.create({
+    key: 'dragon_idle',
+    frames: [
+        { key: 'dragon', frame: 'dragon_knight_idle-0.png' },
+        { key: 'dragon', frame: 'dragon_knight_idle-1.png' }
+    ],
+    frameRate: 8,
+    repeat: -1,
+});
+
+this.anims.create({
+    key: 'dragon_run',
+    frames: [
+        { key: 'dragon', frame: 'dragon_knight_attack_0.png' },
+        { key: 'dragon', frame: 'dragon_knight_attack_1.png' },
+        { key: 'dragon', frame: 'dragon_knight_attack_2.png' },
+        { key: 'dragon', frame: 'dragon_knight_attack_3.png' },
+        { key: 'dragon', frame: 'dragon_knight_attack_4.png' }
+    ],
+    frameRate: 8,
+    repeat: -1,
+});
+
+
+
     cursors = this.input.keyboard.createCursorKeys();
 }
 function update (){
