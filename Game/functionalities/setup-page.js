a53c3adb547980e6a54fed426f24efb4aaf47e67
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-27 23:00:35 +0200

    used localStorage to save user data

--- Game/functionalities/setup-page.js
+++ Game/functionalities/setup-page.js
@@ -1,33 +1,41 @@
 const nextButton1 = document.getElementById("setup-button-1");
 const nextButton2 = document.getElementById("setup-button-2");
-const userNameInput = document.getElementById("username");
-const userNameForm = document.getElementById("usernameForm");
+
 const vikingCharacter = document.getElementById("viking-character");
 const mageCharacter = document.getElementById("mage-character");
 const assassinCharacter = document.getElementById("assassin-character");
-var userName;
 var chosenCharacter;
-// nextButton2.addEventListener("click",function(){
-//     const userValue = userNameInput.value.trim();
-//     userName = userValue;
-//     window.location.href="pages/player-setup/character.html"
-//     console.log("hi")
-    
-// })
 
-vikingCharacter.addEventListener("click", function(){
-    chosenCharacter="viking";
-    localStorage.setItem("chosenCharacter", chosenCharacter);
-    window.location.href="http://localhost:5500/pages/game.html"
-})
-mageCharacter.addEventListener("click", function(){
-    chosenCharacter="mage";
-    localStorage.setItem("chosenCharacter", chosenCharacter);
-    window.location.href="http://localhost:5500/pages/game.html"
+const userNameInput = document.getElementById("username");
+var userName;
 
-})
-assassinCharacter.addEventListener("click", function(){
-    chosenCharacter="assassin";
-    localStorage.setItem("chosenCharacter", chosenCharacter);
-    window.location.href="http://localhost:5500/pages/game.html"
-})
+nextButton1?.addEventListener("click", function () {
+window.location.href = "http://localhost:5500/pages/player-setup/username.html";
+});
+nextButton2?.addEventListener("click", function () {
+    if(userNameInput.value.trim()===""){
+        alert("You must name yourself");
+    }
+    else{
+        const userValue = userNameInput.value.trim();
+        userName = userValue;
+        localStorage.setItem("chosenUsername", userName);
+        window.location.href = "http://localhost:5500/pages/player-setup/character.html";
+    }
+});
+
+vikingCharacter?.addEventListener("click", function () {
+  chosenCharacter = "viking";
+  localStorage.setItem("chosenCharacter", chosenCharacter);
+  window.location.href = "http://localhost:5500/pages/game.html";
+});
+mageCharacter?.addEventListener("click", function () {
+  chosenCharacter = "mage";
+  localStorage.setItem("chosenCharacter", chosenCharacter);
+  window.location.href = "http://localhost:5500/pages/game.html";
+});
+assassinCharacter?.addEventListener("click", function () {
+  chosenCharacter = "assassin";
+  localStorage.setItem("chosenCharacter", chosenCharacter);
+  window.location.href = "http://localhost:5500/pages/game.html";
+});
