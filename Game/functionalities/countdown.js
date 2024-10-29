f86353856c412670a12ec3e89340085e54e49dd3
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-29 17:44:58 +0200

    added logic to check if user finished the game and there is still time to get bonus

--- Game/functionalities/countdown.js
+++ Game/functionalities/countdown.js
@@ -17,9 +17,11 @@ function updateCountdown() {
   if (timeLeft > 0) {
     timeLeft--; // Decrease time left by 1 second
     timerText.setText(formatTime(timeLeft)); // Update the text with formatted time
-  } else {
-    timerText.setText("Time's up!");
-    this.time.removeAllEvents(); // Stop the timer
+  } else if(localStorage.getItem("levelNumber")==="4" && timeLeft>=0) {
+    timeBonus = 1000;}
+    else{
+        timeBonus = 0;
+        this.time.removeAllEvents(); // Stop the timer
   }
 }
 function formatTime(seconds) {
