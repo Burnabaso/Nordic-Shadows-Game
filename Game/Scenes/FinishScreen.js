89d6407d387561ed965f048379092589bd1499d5
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-29 23:40:12 +0200

    created a finishScreen class scene

---
+++ Game/Scenes/FinishScreen.js
@@ -0,0 +1,28 @@
+class FinishScreen extends Phaser.Scene {
+    constructor() {
+        super({ key: 'FinishScreen' });
+    }
+
+    preload() {
+        preloadAssets.call(this);
+    }
+
+    create() {
+        // TODO: replace with designed image
+        let map=this.add.image(350, 350, 'mapLevel3');
+        map.setScale(this.cameras.main.width / map.width);
+
+       
+        createPlayer.call(this);
+        //dragon creation
+     
+        
+        cursors = this.input.keyboard.createCursorKeys();
+        handleScore(this);
+        handleHealth(this);
+    }
+    
+    update() {
+        updatePlayer.call(this);
+    }
+}
