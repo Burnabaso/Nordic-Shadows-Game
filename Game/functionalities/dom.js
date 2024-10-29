00574f7ed71ac28868d9947c8bac3e7787dc20ca
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-29 13:15:00 +0200

    rendered a stat-card to game page using js

---
+++ Game/functionalities/dom.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded",function(){
+   document.body.innerHTML+=`
+        <div class="stat-card">
+            <h2 id="user-name">Username</h2>
+            <h3>Health:</h3>
+            <p id="health-value">100</p>
+            <h3>Score:</h3>
+            <p id="score-value">100</p>
+        </div>
+   ` 
+})
