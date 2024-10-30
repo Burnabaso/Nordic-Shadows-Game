7eca49c12a2215208ab1bd0abc871320a8c130ca
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-30 11:32:02 +0200

    removed unused variable in dom.js

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,6 +1,5 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileMenuContent = document.getElementById("mobile-menu-content");
-var click=false;
 mobileMenu?.addEventListener("click",function(){
     mobileMenuContent?.classList.toggle("hidden")
 })
