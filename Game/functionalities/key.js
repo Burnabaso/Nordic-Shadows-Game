cbe0ce67bbf96790070dfce2ecef5295f0357ac0
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-31 01:17:45 +0200

    removed unused file

--- Game/functionalities/key.js
+++
@@ -1,5 +0,0 @@
-function collectKey(player, keySprite, collider) {
-    // Disable key sprite and destroy the collider
-    keySprite.disableBody(true, true); // Hide and disable the key
-    this.physics.world.removeCollider(collider); // Remove the collider
-}
