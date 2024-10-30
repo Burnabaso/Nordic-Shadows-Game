7827210e794602129824bd7ecf43938a542e92d8
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-10-30 23:37:39 +0200

    fixed bugs

--- Game/Scenes/SecondLevel.js
+++ Game/Scenes/SecondLevel.js
@@ -10,13 +10,13 @@ class SecondLevel extends Phaser.Scene {
 
     create() {
         this.collectedKeys = 0;
-        let map=this.add.image(350, 400, 'mapLevel1');
+        let map=this.add.image(350, 400, 'mapLevel2');
         map.setScale(this.cameras.main.width / map.width);
         let gate=this.physics.add.staticImage(690, 400, 'gate');
         gate.setSize(65,100);
         gate.setScale(0.18);
         this.gate=gate;
-        
+        dragons.length = 0;        
         createPlayer.call(this);
         this.physics.add.collider(this.player, gate);
         dragons.push(new Dragon(this, 400, 400, [
