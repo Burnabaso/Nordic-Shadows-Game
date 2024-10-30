53c7bc1bd6733aa2546f983d32443229d61b5564
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-30 12:04:32 +0200

    scroll to top logic when navbar logo pressed

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,5 +1,9 @@
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileMenuContent = document.getElementById("mobile-menu-content");
+const logoScroll = document.getElementById("logo-scroll-top");
 mobileMenu?.addEventListener("click",function(){
     mobileMenuContent?.classList.toggle("hidden")
+})
+logoScroll?.addEventListener("click",function(){
+    window.scrollTo(0,0)
 })
